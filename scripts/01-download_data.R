803c22c8666471c4bdb2f57240d902c45d603f74
Tanmay Shinde <tanmayshinde1401@gmail.com>
2024-09-26 17:47:51 -0400

    implemented download_data script to obtain relevant datasets

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -24,6 +24,17 @@ datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'
 #### Download data ####
 data <- filter(datastore_resources, row_number()==4) %>% get_resource()
 
+#### Downloading Ward Data ####
+
+# get package
+ward_package <- show_package("6678e1a6-d25f-4dff-b2b7-aa8f042bc2eb")
+
+ward_resources <- list_package_resources("ward-profiles-25-ward-model")
+
+ward_name_data <- filter(ward_resources, row_number()==3) %>% get_resource()
+
+ward_data <- filter(ward_resources, row_number()==2) %>% get_resource()
 
 #### Save data ####
 write_csv(data, "data/raw_data/raw_data.csv") 
+write_csv(ward_name_data, "data/raw_data/raw_ward_data.csv") 
