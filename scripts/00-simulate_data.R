0dad26d1fc1adacffa6a83510857a92136d5eb4f
Tanmay Shinde <tanmayshinde1401@gmail.com>
2024-09-26 17:46:08 -0400

    implemented simulate_data script to get a sample simulation of the expected cleaned dataset

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,28 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Simulates sample data required for the study as shown in other/sketches/dataset.png
+# Author: Tanmay Sachin Shinde
+# Date: 26 September, 2024
+# Contact: tanmay.shinde@mail.utoronto.ca
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+set.seed(304)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+num_observations <- 75
+status_categories <- c("Planning", "Under Construction", "Occupied")
+status_col <- rep(status_categories, times=25)
 
+wards <- rep(1:25, times=3)
+wards_col <- sort(wards)
 
+arh_col <- sample(1:3000, 75, replace=TRUE)
 
+simulated_data <- data.frame(
+  Ward = wards_col,
+  Status = status_col,
+  ARHApproved20117toPresent = arh_col
+)
+
+head(simulated_data)
